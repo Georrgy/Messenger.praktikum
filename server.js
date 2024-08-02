563294e4870cba34363c946a1b8bd15015399fe0
Georrgy <ovsyannikovgeorgy@gmail.com>
2024-08-02 13:24:48 +0200

    NetlifyExspressEngines

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Раздача статических файлов из директории 'dist'
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // Фолбек на index.html для SPA
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
